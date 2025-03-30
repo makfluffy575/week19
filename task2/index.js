891703b84d97cf9794a0a1d272df909447d9baea
makfluffy575 <fluffy.mk575@gmail.com>
2025-03-30 22:36:03 +0300

    init commit

---
+++ task2/index.js
@@ -0,0 +1,27 @@
+let postInfo = "";
+
+let post = [];
+
+const createPost = () => {
+  postInfo += `<div class="postWrapper">
+  <h2 class="postTitle">${post.title}</h2>
+  <p class="postText">${post.body}</p>
+  </div>`
+};
+
+const postContainer = document.querySelector(".postContainer");
+
+const addPost = () => {
+  postContainer.innerHTML = postInfo;
+}
+
+
+fetch ('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(data => {console.log(data);
+  data.forEach(item => {post = item;
+    createPost(item); 
+    addPost()});
+})
+.catch(err => console.log(err));
+
